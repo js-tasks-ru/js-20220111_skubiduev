24d9ecad3a47479505a02bb233c4e53411ecc598
skubiduev <skubiduev@rambler.ru>
2022-01-17 23:02:33 +0300

    ''

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -6,6 +6,9 @@
  */
 export function sortStrings(arr, param = 'asc') {
   const copyOfArr = [...arr];
-  copyOfArr.sort((string1, string2) => string1.localeCompare(string2, ['ru', 'en'], {caseFirst: 'upper'}));
-  return param === 'asc' ? copyOfArr : copyOfArr.reverse();
+  copyOfArr.sort((string1, string2) => {
+    const sortedNumber = string1.localeCompare(string2, ['ru', 'en'], {caseFirst: 'upper'});
+    return param === 'asc' ? sortedNumber : -sortedNumber;
+  });
+  return copyOfArr;
 }
