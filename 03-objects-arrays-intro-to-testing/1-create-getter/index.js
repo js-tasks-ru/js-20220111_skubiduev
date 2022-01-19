c479f20657f52bf8d0645f4ba2f30afdf6f06a36
skubiduev <skubiduev@rambler.ru>
2022-01-19 06:32:09 +0300

    ''

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,18 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const arrOfPath = path.split('.');
 
+  return function (object) {
+    let endingObject = object;
+
+    for (let i = 0; i < arrOfPath.length; i++) {
+      if (endingObject === undefined) {
+        return;
+      } else if (i === arrOfPath.length - 1) {
+        return endingObject[arrOfPath[i]];
+      }
+      endingObject = endingObject[arrOfPath[i]];
+    }
+  };
 }
--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,25 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  let newString = '', countIteration = 0, countMatching = 0;
+  const arrOfSymbols = string.split('');
 
+  if (size === undefined) {
+    return string;
+  }
+
+  for (let i = 0; i < arrOfSymbols.length; i++) {
+    for (let j = i - 1; countIteration < size; j--) {
+      if (arrOfSymbols[j] === arrOfSymbols[i]) {
+        countMatching++;
+      }
+      countIteration++;
+    }
+    if (countMatching < size) {
+      newString += arrOfSymbols[i];
+    }
+    countIteration = countMatching = 0;
+  }
+
+  return newString;
 }
